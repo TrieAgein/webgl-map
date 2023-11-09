d22adfed13d993f48ea88acd25a72021e0e14628
TrieAgein <simonliu.9797@gmail.com>
2023-11-09 13:05:42 -0800

    finished interactability

--- src/app/select/SelectComp.js
+++ src/app/select/SelectComp.js
@@ -1,18 +1,23 @@
 'use client'
 import { Select } from '@radix-ui/themes';
+import React from 'react';
 
-export const SelectComp = ({options, title, defaultValue, value, onChange}) => {
+export const SelectComp = ({options, title, value, onChange}) => {
+    if (!Array.isArray(options)) {
+        options = [];
+      }
+    
     return(
-        <Select.Root defaultValue={defaultValue}  value={value} onValueChange={onChange}>
+        <Select.Root value={value} onValueChange={onChange}>
             <Select.Trigger color="indigo" variant="soft" mr="2" />
             <Select.Content color="indigo">
                 <Select.Group>
                     <Select.Label>{title}</Select.Label>
-                    {options.map((option) => {
-                        return(
-                            <Select.Item value={option}>{option}</Select.Item>
-                        )
-                    })}
+                    {options.map((option) => (
+                    <Select.Item key={option} value={option}>
+                        {option}
+                        </Select.Item>
+                        ))}
                 </Select.Group>
             </Select.Content>
             </Select.Root>
--- src/app/topnav/TopNav.js
+++ src/app/topnav/TopNav.js
@@ -1,20 +1,59 @@
 import { Flex, Text } from '@radix-ui/themes';
 import { SelectComp } from '../select'
+import { useEffect } from 'react'
 
 export const TopNav = ({
     city,
     setCity,
     dataset,
-    setDataset
+    datasetOptions,
+    setDataset,
+    visualOptions,
+    visualizationType,
+    setVisualizationType
 }) => {
-    return(
-        <Flex direction="row" align="center" justify="between" width={'100%'} style={{backgroundColor: 'purple', height: 60, borderBottomWidth: 1, borderColor: 'gray'}}>
-            <Text size="3" weight="bold" ml="3">WebGL Visualization App</Text>
-            <Flex direction="row">
-                <Text style={{padding:5}}>Select Your Dataset:</Text>
-                <SelectComp options={['LA', 'NYC']} title="Cities" defaultValue="LA" value={city} onChange={setCity} />
-                <SelectComp options={['Tree', 'Active Businesses']} title="Dataset" defaultValue="Tree" value={dataset} onChange={setDataset} />
-            </Flex>
+
+    useEffect(() => {
+        // Set the initial value for the dataset based on datasetOptions
+        if (datasetOptions.length > 0) {
+          setDataset(datasetOptions[0]);
+        }
+      }, [city, datasetOptions, setDataset]);
+    
+    return (
+        <Flex
+          direction="row"
+          align="center"
+          justify="between"
+          width={'100%'}
+          style={{ backgroundColor: 'purple', height: 60, borderBottomWidth: 1, borderColor: 'gray' }}
+        >
+          <Text size="3" weight="bold" ml="3">
+            WebGL Visualization App
+          </Text>
+          <Flex direction="row">
+            <Text style={{ padding: 5 }}>Select Your Dataset:</Text>
+            <SelectComp
+              options={['LA', 'NYC']}
+              title="Cities"
+              defaultValue="LA"
+              value={city}
+              onChange={setCity}
+            />
+            <SelectComp
+              options={['scatterplot', 'hexagon']}
+              title="Visual"
+              defaultValue="scatterplot"
+              value={visualizationType}
+              onChange={setVisualizationType}
+            />
+            <SelectComp
+              options={datasetOptions}
+              title="Dataset"
+              value={dataset}
+              onChange={setDataset}
+            />
+          </Flex>
         </Flex>
-    )
-}
+      );
+};
